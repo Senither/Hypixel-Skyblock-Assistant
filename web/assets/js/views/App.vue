cd179528d632bd77b7508a2f18bac9138699ebaf
Alexis Tan <alexis@sen-dev.com>
2020-03-21 15:28:16 +0100

    Add footer to the main app view

--- web/assets/js/views/App.vue
+++ web/assets/js/views/App.vue
@@ -10,8 +10,23 @@
             v-if="this.stage == this.stages.HAS_REPORT"
             :report="report"
         />
+
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    Created by <a href="https://senither.com/">Alexis Tan</a>, powered by <a href="https://bulma.io/">Bulma</a> and <a href="https://vuejs.org/">VueJS</a>, theme by <a href="https://jenil.github.io/bulmaswatch/">Bulmaswatch</a>.
+                    <br>Get the <a href="https://github.com/Senither/Hypixel-Skyblock-Assistant/">source code</a> on <a href="https://github.com/Senither/Hypixel-Skyblock-Assistant/">GitHub</a>.
+                </p>
+            </div>
+        </footer>
     </div>
 </template>
+
+<style lang="scss">
+    footer.footer {
+        margin-top: 48px;
+    }
+</style>
 
 <script>
     import LoadingReport from './LoadingReport';
